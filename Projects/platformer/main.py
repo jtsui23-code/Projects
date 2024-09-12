33de26de11b6c70c25cb09cbf66d3d4011019fce
Jack <Jacktsui23@gmail.com>
2024-09-12 14:59:26 -0500

    added collsion

--- Projects/platformer/main.py
+++ Projects/platformer/main.py
@@ -28,6 +28,11 @@ class game:
         # up is bound to [0] down is bound to [1] 
         self.movement = [False, False, False, False]
 
+        # this creates an rectable object 
+        # the parameters are (x location, y location, width of rect,
+        #  height of rect)
+        self.collisionArea = pygame.Rect(200, 300, 300, 60)
+
     def run(self):
         while True:
 
@@ -47,6 +52,23 @@ class game:
             # (0,0) starts on top left like reading english
             self.screen.blit(self.img, self.imgPos)
 
+            # creates imageRect object
+            # the * unpacks all of the elements in the list/array
+            # self.imgPos has two values and self.img.get_size() 
+            # has another two values
+            # so this is the same as 
+            # pygame.Rect(self.imgPos[0], self.imgPos[1], self.img.
+            # get_width, self.img.get_height)
+            imgR = pygame.Rect(*self.imgPos, * self.img.get_size())
+
+            
+            if imgR.colliderect(self.collisionArea):
+                #this changes the color of the collision area if you collide into
+                # it as the cloud object
+                pygame.draw.rect(self.screen, (200, 130, 88), self.collisionArea)
+            else:
+                pygame.draw.rect(self.screen, (80, 150, 200), self.collisionArea)
+
             # pygame.event.get() gets the user's input
             for event in pygame.event.get():
                 #checks if the user pressed x button on top right
@@ -64,6 +86,19 @@ class game:
                     # you set movement[1] to true because if both w & s
                     # are pressed then there is no movement
                         self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+
+                    if event.key == pygame.K_a:
+                        self.movement[2] = True
+                    if event.key == pygame.K_d:
+                        self.movement[3] = True
+                    if event.key == pygame.K_LEFT:
+                        self.movement[2] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[3] = True
                 
                 # if you let go of the key
                 if event.type == pygame.KEYUP:
@@ -73,15 +108,18 @@ class game:
                         self.movement[0] = False
                     if event.key == pygame.K_s:
                         self.movement[1] = False
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_a:
-                        self.movement[2] = True
-                    if event.key == pygame.K_d:
-                        self.movement[3] = True
-                if event.type == pygame.KEYUP:
+                    
                     if event.key == pygame.K_a:
                         self.movement[2] = False
                     if event.key == pygame.K_d:
+                        self.movement[3] = False
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+                    if event.key == pygame.K_LEFT:
+                        self.movement[2] = False
+                    if event.key == pygame.K_RIGHT:
                         self.movement[3] = False
 
             # Updates the screen
