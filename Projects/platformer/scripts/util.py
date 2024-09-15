7b5f897c800eff7e729d86743d5809c098a33b40
Jack <Jacktsui23@gmail.com>
2024-09-15 14:04:26 -0500

    copy animation

--- Projects/platformer/scripts/util.py
+++ Projects/platformer/scripts/util.py
@@ -30,3 +30,10 @@ class animation():
         # checks if animation is finished
         self.done = False
         self.frame = 0
+    
+    # copying animations to be used in many instances
+    # will conserve memory since self.images is passed by reference
+    # so there are not many copies of self.image list/array
+    def copy(self):
+        return animation(self.images, self.imageDur,self.loop)
+    
