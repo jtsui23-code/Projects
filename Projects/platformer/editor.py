82973887ed85edbe8b1d05ed7050faace5de39e0
Jack <Jacktsui23@gmail.com>
2024-09-22 22:03:39 -0500

    debugging moue position

--- Projects/platformer/editor.py
+++ Projects/platformer/editor.py
@@ -81,8 +81,8 @@ class editor:
 
             # current position/pixel is scaled up so would
             # give wrong cordinates so have to convert back normal pixels
-            mousePos = (mousePos[0] / (self.screen.get_height() * self.screen.get_width()), 
-                        mousePos[1] / (self.screen.get_height() * self.screen.get_width()))
+            mousePos = (mousePos[0] / RENDERSCALE, 
+                        mousePos[1] / RENDERSCALE)
 
             # .blit([what you want to render], [where you want to render])
             self.display.blit(currentTileImg, (5,5))
