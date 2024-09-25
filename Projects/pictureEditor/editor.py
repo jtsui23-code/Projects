6c931fd7686e4f055e6871fb21e06b2b59f527d9
Jack <Jacktsui23@gmail.com>
2024-09-25 15:07:27 -0500

    added prompt for width and height

--- Projects/pictureEditor/editor.py
+++ Projects/pictureEditor/editor.py
@@ -13,10 +13,15 @@ if __name__ == "__main__":
 
     fileName = str(input("What is the name of the picture include file type extension:"))
 
+    width = int(input("What do you want the new width to be:"))
+
+    height = int(input("What do you want the new height to be:"))
     # Gives home path of user i.e C:Users\Natck\
     homePath = os.path.expanduser("~")
 
     imgPath = os.path.join(homePath, "Downloads", fileName)
 
     newPath = os.path.join(homePath, "OneDrive", "Pictures", "Saved Pictures")
+
+    resolution()
 
