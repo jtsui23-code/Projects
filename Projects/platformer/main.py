155350a797725a83b2b072cb8e1f02ba4a77df60
Jack <Jacktsui23@gmail.com>
2024-10-21 21:15:09 -0500

    debugging spawner

--- Projects/platformer/main.py
+++ Projects/platformer/main.py
@@ -52,7 +52,7 @@ class game:
 
         self.clouds = cloudz(self.assets['clouds'], count=16)
         
-        self.player = player(self, (100,20), (8, 15))
+        self.player = player(self, (50,50), (8, 15))
 
         self.tilemap = tilemap(self, tilesize=16)
         self.tilemap.load('levels/map.json')
