ac47a2ac9fe7284f78934f055d7e80c452b142ab
Jack <Jacktsui23@gmail.com>
2024-10-24 14:19:26 -0500

    creating the render method in the spark script

--- Projects/platformer/scripts/spark.py
+++ Projects/platformer/scripts/spark.py
@@ -10,3 +10,12 @@ class Spark:
     def update(self):
         self.pos[0] += math.cos(self.angle) * self.speed
         self.pos[1] += math.sin(self.angle) * self.speed
+
+        self.speed = max(0, self.speed - 1)
+        return not self.speed
+
+    def render(self, surf, offset=(0,0)):
+        renderPoints = [
+            (self.pos[0] + math.cos(self.angle * self.speed))
+        ]
+        pygame.draw.polygon(surf, (255, 255, 255), renderPoints)
