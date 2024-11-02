474dca56bd69b1ca56ea1c3e7e3ca3ea6387c617
Jack <Jacktsui23@gmail.com>
2024-11-02 14:51:29 -0500

    debugging the free fall checker

--- Projects/platformer/scripts/beings.py
+++ Projects/platformer/scripts/beings.py
@@ -282,9 +282,12 @@ class Player(physicsBeing):
             # toggle jumping animation
             if self.velocity[1] < 0:
                 self.setAction('jump')
+                
+            # checks if the player is falling 
+            if self.velocity[1] > 0:
                 self.airTime += 1
 
-            if self.airTime > 5:
+            if self.airTime > 10:
                 self.game.dead += 1
             # if the player is not standing still
             # horizontally, then toggle running animation
