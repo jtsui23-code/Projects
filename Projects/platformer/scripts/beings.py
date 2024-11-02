e98efef93a14068950122b74e14eb3e835b2ca3a
Jack <Jacktsui23@gmail.com>
2024-11-02 15:13:37 -0500

    adding air time counter using the frame counter to control the amount of imcrements

--- Projects/platformer/scripts/beings.py
+++ Projects/platformer/scripts/beings.py
@@ -283,6 +283,13 @@ class Player(physicsBeing):
             # toggle jumping animation
             if self.velocity[1] < 0:
                 self.setAction('jump')
+            
+            if self.velocity[1] != 0:
+                if pygame.time.get_ticks() >= 60:
+                    self.airTime += 1
+            
+            if self.airTime >= 10:
+                self.game.dead += 1
                 
             # if the player is not standing still
             # horizontally, then toggle running animation
