88a1296f12db7820e256c2bfb0b4936aaf21befc
Jack Tsui <jacktsui23@gmail.com>
2024-12-18 15:57:07 -0600

    Changed names and made game, character, and player class

---
+++ Projects/RogueLike/_6_1MakingGameClass.py
@@ -0,0 +1,72 @@
+import pygame   # Importing Pygame library
+import sys      # Importing sys library to properly close the game
+import math     # Import for physics/movement
+from _6_2MakingCharacterClass import Player
+
+class Game:
+    def __init__(self):
+        # Starts up Pygame
+        pygame.init()
+        
+        # Setting the game window's dimensions
+        self.screen_width = 800
+        self.screen_height = 600
+        
+        # Creating a screen variable with the window dimension variables set above
+        # when setting window dimensions have to do .set_mode( (_,_) )
+        # Treat the (_,_) as order pairs inside of ( (_,_) )
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        
+        # Sets the name of the window icon to "Rogue-like"
+        pygame.display.set_caption("Rogue-Like")
+        
+        # Sets the background color based of of RGB (255,255,255)
+        # the color is black is (0,0,0)
+        self.background_color = (0, 0, 0)
+        
+        # Set framerate for game
+        self.clock = pygame.time.Clock()
+        
+        self.player = Player()
+        self.running = True
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            # Checks specifically for input on the exit button 
+            # On top right of window
+            if event.type == pygame.QUIT:
+                # Stops the game if the user clicks the exit button
+                pygame.quit()
+                sys.exit()
+                self.running = False
+            
+            self.player.handle_input(event)
+
+    def update(self):
+        self.player.update_position()
+
+    def render(self):
+        # Fills the window with the background color
+        self.screen.fill(self.background_color)
+        
+        # .blit() draws whatever object on the window
+        # in this case screen is the main game window 
+        # The .blit is used as followed
+        # .blit( [Object want to draw on window], (x - position of object on window, y - position of object on window) )
+        # Note that the position of the object is an ordered pair (x,y) inside of .blit(player, (x,y) )
+        self.screen.blit(self.player.sprite, (self.player.position[0], self.player.position[1]))
+        
+        # Updates every single window might want to 
+        # change to pygame.display.update() to only update one window
+        pygame.display.flip()
+
+    def run(self):
+        while self.running:
+            self.clock.tick(60)
+            self.handle_events()
+            self.update()
+            self.render()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
