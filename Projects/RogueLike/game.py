e1ee964ce9caebc015a7688d8c1ce707506f8168
Jack Tsui <jacktsui23@gmail.com>
2024-12-19 15:05:26 -0600

    Finished recreating game class now starting on the tilemap class

--- Projects/RogueLike/game.py
+++ Projects/RogueLike/game.py
@@ -29,11 +29,8 @@ class Game:
         self.movement = [False, False]
         
         self.assets = {
-            'decor': load_images('tiles/decor'),
-            'grass': load_images('tiles/grass'),
-            'large_decor': load_images('tiles/large_decor'),
-            'stone': load_images('tiles/stone'),
-            'player': load_image('entities/player.png')
+            
+            'player': load_image('Player/player.png')
         }
         
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
@@ -74,13 +71,17 @@ class Game:
                         self.movement[1] = True
                     if event.key == pygame.K_UP:
                         self.player.velocity[1] = -3
+
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
             
+            
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+
+
             pygame.display.update()
             self.clock.tick(60)
 
