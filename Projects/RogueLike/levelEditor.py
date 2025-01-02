a26cdfee2b6b96edbaa9b4d3ef4b35cb7991812f
Jack Tsui <jacktsui23@gmail.com>
2025-01-01 20:21:33 -0600

    Changing the image of the basic slash attack.

--- Projects/RogueLike/levelEditor.py
+++ Projects/RogueLike/levelEditor.py
@@ -10,7 +10,7 @@ from tilemap import Tilemap
 # Caution when cauculating the mouse position have to account for this 
 # RENDERSCALE as well or the tiles will not be placed where the mouse
 # clicks on the level editor.
-RENDERSCALE = 2.0
+RENDERSCALE = 2.9
 class LevelEditor:
 
     # Sets up the game windows, framerate, player, and other assets
