4ae6835cd36e05e63800e0f3318d79b500574e7b
Jack Tsui <jacktsui23@gmail.com>
2025-01-08 14:45:34 -0600

    Debugging slash attack.

--- Projects/RogueLike/character.py
+++ Projects/RogueLike/character.py
@@ -223,12 +223,12 @@ class Player(Character):
             # to create a full slash attack while (swingArc * swingProgress) increments the slash attack 
             # based on the attack frames.
             if not self.attackFlip:
-                currentAngle = self.attackAngle + (swingArc / 2)  - (swingArc * swingProgress)
+                currentAngle = self.attackAngle - (swingArc / 2)  + (swingArc * swingProgress)
 
             # When the slash attack is flipped facing the left then the signs are flipped becasuse 
             # of the slash attack is directed in the opposite direction.
             else:
-                currentAngle = self.attackAngle - (swingArc / 2)  + (swingArc * swingProgress)
+                currentAngle = self.attackAngle + (swingArc / 2)  - (swingArc * swingProgress)
 
 
             # offsets are for where the hitboxs should be located during the swing attack
